b5b05265dd3f6f7114ba2b2d9dd984a5c8fd5873
Raman <rzv090701@gmail.com>
2024-10-29 22:29:36 -0400

    added l2 regularization & increased num epochs

--- demo.py
+++ demo.py
@@ -84,6 +84,7 @@ def train_lora(
         max_grad_norm=0.3,
         warmup_ratio=0.05,
         lr_scheduler_type="cosine",
+        weight_decay=0.01,
     )
     tokenizer = AutoTokenizer.from_pretrained(
         model_id,
